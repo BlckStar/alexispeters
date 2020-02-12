b0e2be0f9932dd67d524ab60dd30002c464d262c
Alexis Peters <alexis.peters@jungheinrich.de>
2020-02-12 12:06:46 +0100

    add some logos and footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
+import styles from './Footer.module.css';
+import GithubLogo from '../Assets/GitHub-Mark-32px.png';
+import StackoverFlowLogo from '../Assets/stackoverflow.svg';
+import XingLogo from '../Assets/xing.svg';
+import Legal from './Legal/Legal';
+
 const Footer = () => {
-    return <div>foo</div>
+    return <div className={styles.Footer}>
+        <div className={styles.FooterContent}>
+            <div className={styles.Contact}>
+                <p>
+                    Alexis Peters<br />
+                    Bebelstr. 3<br />
+                    21614 Buxtehude<br />
+                    info@alexispeters.de
+                </p>
+            </div>
+            <div className={styles.SocialMedia}>
+                <a href="https://github.com/BlckStar">
+                    <img src={GithubLogo} />
+                </a>
+                <a href="https://stackoverflow.com/users/4279830/alexis-peters">
+                    <img src={StackoverFlowLogo} />
+                </a>
+                <a href="https://www.xing.com/profile/Alexis_Peters2/cv">
+                    <img src={XingLogo} />
+                </a>
+            </div>
+        </div>
+        <Legal />
+    </div>
 }
 
 export default Footer;
